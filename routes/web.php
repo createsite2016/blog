7c9d9ffd557e65fe48035bb9c143f128fa5399fe
Galushko Andrey <createsite2016@gmail.com>
2021-11-14 02:12:09 +0300

    Интегрировал вёрстку админпанели, добавил контроллер и маршрут для админпанели

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\Main\IndexController as AdminIndexController;
 use App\Http\Controllers\Main\IndexController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', IndexController::class);
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', AdminIndexController::class);
+});
 
 
 Auth::routes();

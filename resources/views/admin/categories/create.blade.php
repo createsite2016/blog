270b97c7898652cfb4355480082eaa572a07dc75
Galushko Andrey <createsite2016@gmail.com>
2021-11-14 04:49:43 +0300

    создал вью и контроллер создания категории

---
+++ resources/views/admin/categories/create.blade.php
@@ -0,0 +1,44 @@
+@extends('admin.layouts.main')
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Добавление категории</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Starter Page</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <form action="{{ route('admin.category.store') }}" method="POST" class="w-25">
+                            @csrf
+                            <div class="form-group">
+                                <input type="text" name="title" class="form-control" placeholder="Название категории">
+                            </div>
+                            <input type="submit" class="btn btn-primary" value="Добавить">
+                        </form>
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection

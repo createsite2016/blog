0e0c5c001171235a2c73c8610e630b63a602b9a5
Galushko Andrey <createsite2016@gmail.com>
2021-11-15 02:50:01 +0300

    во вью добавил поля формы, для загрузки картинок поста

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -26,7 +26,7 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-12">
-                        <form action="{{ route('admin.post.store') }}" method="POST">
+                        <form action="{{ route('admin.post.store') }}" method="POST" enctype="multipart/form-data">
                             @csrf
                             <div class="form-group w-25">
                                 <input type="text" name="title" class="form-control" placeholder="Название поста"
@@ -44,6 +44,30 @@
                                     Это поле необходимо заполнить
                                 </div>
                                 @enderror
+                            </div>
+                            <div class="form-group w-50">
+                                <label for="exampleInputFile">Добавить превью</label>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" name="preview_image">
+                                        <label class="custom-file-label">Выберите изображение</label>
+                                    </div>
+                                    <div class="input-group-append">
+                                        <span class="input-group-text">Загрузка</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group w-50">
+                                <label for="exampleInputFile">Добавить главное изображение</label>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" name="main_image">
+                                        <label class="custom-file-label">Выберите изображение</label>
+                                    </div>
+                                    <div class="input-group-append">
+                                        <span class="input-group-text">Загрузка</span>
+                                    </div>
+                                </div>
                             </div>
                             <input type="submit" class="btn btn-primary" value="Добавить">
                         </form>
